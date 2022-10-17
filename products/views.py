1e536c7792d43bb43f88ca681311a1164e235652
erikandersson96 <erik96.andersson@gmail.com>
2022-10-17 13:48:34 +0000

    Added a view for render products template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """
+    A view to render all watch products,
+    including sorting and search queries.
+    """
+
+    return render(request, 'products/products.html')
