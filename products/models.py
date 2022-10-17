79015bb3090bbc4d8903b22250085f4adc5fec63
erikandersson96 <erik96.andersson@gmail.com>
2022-10-17 12:40:02 +0000

    Added Product model to products app

---
+++ products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Product(models.Model):
+    """
+    Model for products
+    """
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    watch_make = models.CharField(max_length=254)
+    watch_model = models.CharField(max_length=254)
+    description = models.TextField()
+    watch_size = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.watch_model
