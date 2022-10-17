9423ddb9984ecd497893abf379783b7444eb32a5
erikandersson96 <erik96.andersson@gmail.com>
2022-10-17 12:59:14 +0000

    Changed decimal places to 3 for price

--- products/models.py
+++ products/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     watch_model = models.CharField(max_length=254)
     description = models.TextField()
     watch_size = models.IntegerField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=3)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
