bd75f882e4f86d302644204de530261432662d39
erikandersson96 <erik96.andersson@gmail.com>
2022-10-18 10:21:20 +0000

    Added a view for product_detail & testcase of view

--- products/test_views.py
+++ products/test_views.py
@@ -8,10 +8,17 @@ from django.urls import reverse
 class TestViews(TestCase):
     """ Unit test for products app Views """
 
-    def test_products_is_resolved(self):
+    def test_all_products_is_resolved(self):
         """ Test products page renders correct page """
 
         url = reverse('products')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_product_detail_is_resolved(self):
+        """ Test products page renders correct page """
+
+        url = reverse('products')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
