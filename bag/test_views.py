c26c628acba2055004504809bf157765587de221
erikandersson96 <erik96.andersson@gmail.com>
2022-10-24 12:09:34 +0000

    Added bag urls.py, & testcase for bag views url

---
+++ bag/test_views.py
@@ -0,0 +1,16 @@
+"""
+Home app test for Views
+"""
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestViews(TestCase):
+    """ Unit test for bag app Views """
+
+    def test_bag_is_resolved(self):
+        """ Test bag page renders correct page """
+
+        url = reverse('view_bag')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
