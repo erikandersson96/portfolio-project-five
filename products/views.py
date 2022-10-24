32eee01d78fe5b91b0b304c1584888659b622552
erikandersson96 <erik96.andersson@gmail.com>
2022-10-24 07:41:40 +0000

    Added functionality, sorting products by price

--- products/views.py
+++ products/views.py
@@ -11,8 +11,23 @@ def all_products(request):
     """
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,9 +38,12 @@ def all_products(request):
             queries = Q(watch_make__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
