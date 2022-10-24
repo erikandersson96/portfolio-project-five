4e5b081a8fea7a87f780d99c8edcdb0d638bd455
erikandersson96 <erik96.andersson@gmail.com>
2022-10-24 09:14:11 +0000

    Changed sortkey to watch_make instead of name

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product
 
 
@@ -18,10 +20,9 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'name':
+            if sortkey == 'watch_make':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-
+                products = products.annotate(lower_name=Lower('watch_make'))
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
