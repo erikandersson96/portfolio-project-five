d0e5926a7f4e7d7ddd576881230d32ad550387c4
erikandersson96 <erik96.andersson@gmail.com>
2022-10-27 11:43:08 +0000

    Added grand total to context processor contexts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -5,11 +7,25 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'grand_total': grand_total,
     }
 
     return context
