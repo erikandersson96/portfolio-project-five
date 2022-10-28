e91e2de4b09df8e15cdb4959538a7bd67c475544
erikandersson96 <erik96.andersson@gmail.com>
2022-10-28 10:45:39 +0000

    Changed max quantity to 10

--- bag/views.py
+++ bag/views.py
@@ -15,7 +15,6 @@ def add_to_bag(request, item_id):
     """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
