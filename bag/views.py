bbfdb9cd21ee3f16fd1d62255ed4cd4fcddb3dd5
erikandersson96 <erik96.andersson@gmail.com>
2022-10-28 09:40:43 +0000

    Deleted selector for size, added size stored in db

--- bag/views.py
+++ bag/views.py
@@ -15,6 +15,7 @@ def add_to_bag(request, item_id):
     """
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
