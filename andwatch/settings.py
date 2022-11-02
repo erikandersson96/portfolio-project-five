025e6f21050bd8d03674b5e9fdd83341306881f3
erikandersson96 <erik96.andersson@gmail.com>
2022-11-02 12:09:39 +0000

    Added checkout template structure & install crispy

--- andwatch/settings.py
+++ andwatch/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'checkout',
 
     # Other
+    'crispy_forms',
     'storages',
 ]
 
@@ -68,6 +69,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'andwatch.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
