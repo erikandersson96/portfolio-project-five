48972b6ac1fa42ba8a758ad7786de46dc49c02d6
erikandersson96 <erik96.andersson@gmail.com>
2022-11-02 15:01:35 +0000

    Added stripe elements & styling to match checkout

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LzhQUHjG9DDmtCup63DaDHDKfyuoVEz5JInhFA1IV2mMve56TH8ElBjrienWZlC3atyv3o2gnBP4mv1WnYjBTkI00yT4kjIcd',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
