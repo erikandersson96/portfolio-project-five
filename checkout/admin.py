ccb92bd2093adb9d3b99189e7d421356d95d34be
erikandersson96 <erik96.andersson@gmail.com>
2022-11-10 08:28:23 +0000

    Added tables user profile page & save user info

--- checkout/admin.py
+++ checkout/admin.py
@@ -17,7 +17,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'order_total', 'grand_total',
                        'original_bag', 'stripe_pid',)
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address',
               'county', 'order_total', 'grand_total',
