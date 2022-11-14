2d3467e3f510e2ac6dd3ed209c1dbf737170beeb
erikandersson96 <erik96.andersson@gmail.com>
2022-11-14 12:49:42 +0000

    Added 403 error template, view and url

--- andwatch/views.py
+++ andwatch/views.py
@@ -6,3 +6,10 @@ def handler404(request, exception):
     A view for Error 404 - Page Not Found
     """
     return render(request, "errors/404.html", status=404)
+
+
+def handler403(request, exception):
+    """
+    A view for Error 403 - Forbidden resource
+    """
+    return render(request, "errors/403.html", status=403)
