1bc8966ea15c0f722a6768fad41e0c2229b71a58
erikandersson96 <erik96.andersson@gmail.com>
2022-11-24 13:42:24 +0000

    Fix issue with commas causing error for checkout

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -69,14 +69,12 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_phone_number = shipping_details.phone,
-                profile.default_country = shipping_details.address.country,
-                profile.default_postcode = \
-                    shipping_details.address.postal_code,
-                profile.default_town_or_city = shipping_details.address.city,
-                profile.default_street_address = \
-                    shipping_details.address.line1,
-                profile.default_county = shipping_details.address.state,
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address = shipping_details.address.line1
+                profile.default_county = shipping_details.address.state
                 profile.save()
 
         order_exists = False
